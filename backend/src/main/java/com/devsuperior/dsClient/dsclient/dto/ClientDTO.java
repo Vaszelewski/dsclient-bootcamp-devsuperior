b13b16bcd8e86cf36dbf0caed76dd5f3fadc69ec
Everton Vaszelewski <everton.vaszelewski@gmail.com>
2021-01-29 20:37:42 -0300

    Client DTO

---
+++ backend/src/main/java/com/devsuperior/dsClient/dsclient/dto/ClientDTO.java
@@ -0,0 +1,89 @@
+package com.devsuperior.dsClient.dsclient.dto;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import com.devsuperior.dsClient.dsclient.entities.Client;
+
+public class ClientDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double incomeDouble;
+	private Instant birthDate;
+	private Integer children;
+	
+	public ClientDTO() {
+	}
+
+	public ClientDTO(Long id, String name, String cpf, Double incomeDouble, Instant birthDate, Integer children) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.incomeDouble = incomeDouble;
+		this.birthDate = birthDate;
+		this.children = children;
+	}
+	
+	public ClientDTO(Client entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+		this.cpf = entity.getCpf();
+		this.incomeDouble = entity.getIncomeDouble();
+		this.birthDate = entity.getBirthDate();
+		this.children = entity.getChildren();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Double getIncomeDouble() {
+		return incomeDouble;
+	}
+
+	public void setIncomeDouble(Double incomeDouble) {
+		this.incomeDouble = incomeDouble;
+	}
+
+	public Instant getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Instant birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+	
+}
--- backend/src/main/java/com/devsuperior/dsClient/dsclient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dsClient/dsclient/services/ClientService.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dsClient.dsclient.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dsClient.dsclient.dto.ClientDTO;
 import com.devsuperior.dsClient.dsclient.entities.Client;
 import com.devsuperior.dsClient.dsclient.repositories.ClientRepository;
 
@@ -16,7 +18,12 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Client> findAll(){
-		return repository.findAll();
+	public List<ClientDTO> findAll(){
+		List<Client> list = repository.findAll();
+		List<ClientDTO> listDto = new ArrayList<>();
+		for(Client cli : list){
+			listDto.add(new ClientDTO(cli));
+		}
+		return listDto;
 	}
 }
